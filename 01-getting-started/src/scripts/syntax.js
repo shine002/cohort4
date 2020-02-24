36d99ec8fb4cc5610df05c7b301ecd1dd371b076
Shine <shine002@gmail.com>
2020-02-24 13:51:11 -0700

    Syntax file updated

---
+++ 01-getting-started/src/scripts/syntax.js
@@ -0,0 +1,33 @@
+// define attributes / variables
+// number
+function IsAnumber(number){
+    return true
+}
+
+// string
+// boolean
+// array
+// dictionary / objects
+// undefined
+
+// sample if / else
+
+// functions
+// parameters
+// returns
+
+// arrays
+// add to the front
+// add to the end
+// update values
+// loops 
+
+// for
+// for/in
+// while
+// do while
+// forEach (with array and function)
+
+// Objects / Dictionaries
+// declare object
+// lookup key to retrieve value
