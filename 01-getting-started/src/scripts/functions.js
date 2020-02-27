3ee1d89e8f42208cc1bc359679907df57d1558a2
SH!NE <shine002@gmail.com>
2020-02-27 13:15:44 -0700

    Calculater added

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -1,10 +1,18 @@
 
 const functions = {
     
-    size: (num) => {
-        if (num < 10) return "small";
-        if (num < 20) return "medium";
-        return "large";
+    // size: (num) => {
+    //     if (num < 10) return "small";
+    //     if (num < 20) return "medium";
+    //     return "large";
+    // },
+    // isEven:(input) => { 
+    //     return (input%2 == 0);    
+    // },
+
+// Calulator Part
+    isAnumber: (num1, num2) => {
+        return(typeof num1 === 'number' && typeof num2 === 'number');
     },
 
     add: (num1, num2) => {
@@ -14,9 +22,16 @@ const functions = {
     subtract: (num1, num2) => {
         return num1 - num2;
     },
-    isEven:(input) => { 
-         return (input%2 == 0);    
+
+    multiply: (num1, num2) => {
+        return num1 * num2;
     },
+    divide: (num1, num2) => {
+        return (num1 / num2);
+    },
+
+    
+    
 };
 
 export default functions;
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -1,20 +1,50 @@
 import functions from './functions'
 
-test('Check the sizes', () => {
-    expect(functions.size(-1)).toBe("small"); // Consider the edge cases
-    expect(functions.size(0)).toBe("small");
-    expect(functions.size(10)).toBe("medium");
-    expect(functions.size(15)).toBe("medium");
-    expect(functions.size(20)).toBe("large");
-    expect(functions.size(2000000)).toBe("large");
+// test('Check the sizes', () => {
+//     expect(functions.size(-1)).toBe("small"); // Consider the edge cases
+//     expect(functions.size(0)).toBe("small");
+//     expect(functions.size(10)).toBe("medium");
+//     expect(functions.size(15)).toBe("medium");
+//     expect(functions.size(20)).toBe("large");
+//     expect(functions.size(2000000)).toBe("large");
+// });
+
+// test('Does that add function work?', () => {
+//     expect(functions.add(1,2)).toBe(3);
+//     expect(functions.add(101,202)).toBe(303);
+// });
+
+// test("Is the number even", () => {
+//     expect(functions.isEven(2)).toBe(true);
+//     expect(functions.isEven(1)).toBe(false);
+// });
+
+// Calculator part
+
+test('Check if inputs is a number', () => {
+    expect(functions.isAnumber("Shine",2)).toBe(false);  
+    expect(functions.isAnumber(1,2)).toBe(true);
+    expect(functions.isAnumber("Shine","x")).toBe(false); 
 });
 
-test('Does that add function work?', () => {
-    expect(functions.add(1,2)).toBe(3);
-    expect(functions.add(101,202)).toBe(303);
+test ('Add check', () => {
+    expect(functions.add(4,2)).toBe(6);   
+
 });
 
-test("Is the number even", () => {
-    expect(functions.isEven(2)).toBe(true);
-    expect(functions.isEven(1)).toBe(false);
+test ('Subtract check', () => {
+    expect(functions.subtract(4,2)).toBe(2);   
+    expect(functions.subtract(2,-2)).toBe(4); 
+
+});
+
+test ('Mulitply check', () => {
+    expect(functions.multiply(4,2)).toBe(8);   
+    expect(functions.multiply(2,-2)).toBe(-4); 
+});
+
+
+test ('Divide check', () => {
+    expect(functions.divide(4,2)).toBe(2);   
+    expect(functions.divide(2,2)).toBe(1); 
 });
