cda2bb581b43740370e0b300ca1c10c3bb787b72
Shine <shine002@gmail.com>
2020-02-29 11:01:00 -0700

    test

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -1,40 +1,50 @@
 
-var num1 = document.getElementById("num1").value
-var num2 = document.getElementById("num2").value
+// var num1 = document.getElementById("num1").value
+// var num2 = document.getElementById("num2").value
+
+printtext = () => {
+    console.log("Print")
+}
+
+document.getElementById("sub").addEventListener("click", function(){
+    document.getElementById("result").innerHTML = "Hello World";
+});
 
 
 
-const functions = {
+
+
+
+// const functions = {
     
-    // size: (num) => {
-    //     if (num < 10) return "small";
-    //     if (num < 20) return "medium";
-    //     return "large";
-    // },
-    // isEven:(input) => { 
-    //     return (input%2 == 0);    
-    // },
+//     // size: (num) => {
+//     //     if (num < 10) return "small";
+//     //     if (num < 20) return "medium";
+//     //     return "large";
+//     // },
+//     // isEven:(input) => { 
+//     //     return (input%2 == 0);    
+//     // },
 
-// Calulator Part
-    isAnumber: (num1, num2) => {
-        return(typeof num1 === 'number' && typeof num2 === 'number');
-    },
+// // Calulator Part
+//     isAnumber: (num1, num2) => {
+//         return(typeof num1 === 'number' && typeof num2 === 'number');
+//     },
 
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
+//     add: (num1, num2) => {
+//         return num1 + num2;
+//     },
 
-    subtract: (num1, num2) => {
-        return num1 - num2;
-    },
+//     subtract: (num1, num2) => {
+//         return num1 - num2;
+//     },
 
-    multiply: (num1, num2) => {
-        return num1 * num2;
-    },
-    divide: (num1, num2) => {
-        return (num1 / num2);
-    },   
-};
+//     multiply: (num1, num2) => {
+//         return num1 * num2;
+//     },
+//     divide: (num1, num2) => {
+//         return (num1 / num2);
+//     },   
+// };
 
-export default functions;
-
+// // export default functions;
