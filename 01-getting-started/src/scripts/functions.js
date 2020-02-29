ca19c2e453c10d7a98d721f17cb0460c0c893a1a
Shine <shine002@gmail.com>
2020-02-29 13:40:17 -0700

    Calculator finished

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -5,46 +5,64 @@
 
 const functions = {
     
-    // size: (num) => {
-    //     if (num < 10) return "small";
-    //     if (num < 20) return "medium";
-    //     return "large";
-    // },
-    // isEven:(input) => { 
-    //     return (input%2 == 0);    
-    // },
-
-// Calulator Part
-    isAnumber: (num1, num2) => {
-        return(typeof num1 === 'number' && typeof num2 === 'number');
+    size: (num) => {
+        if (num < 10) return "small";
+        if (num < 20) return "medium";
+        return "large";
+    },
+    isEven:(input) => { 
+        return (input%2 == 0);    
     },
 
+// Calulator Part
+    
     add: (num1, num2) => {
-        result = num1 + num2;
-        document.getElementById("result").innerHTML = result;
-
+        var result = parseInt(num1) +  parseInt(num2);
+        document.getElementById("result").value = result;
+        return result;
     },
 
     subtract: (num1, num2) => {
-        var result= num1 - num2;
+        var result = parseInt(num1) - parseInt(num2);
+        document.getElementById("result").value = result;
         return result;
     },
 
     multiply: (num1, num2) => {
-        var result = num1 * num2;
-        re
+        var result = parseInt(num1) * parseInt(num2);
+        document.getElementById("result").value = result;
+        return result;
     },
+    
     divide: (num1, num2) => {
-        return (num1 / num2);
+        var result = parseInt(num1) / parseInt(num2);
+        document.getElementById("result").value = result;
     },   
 };
 
-// export default functions;
-var num1 = document.getElementById("num1")
-var num2 = document.getElementById("num2")
+
+
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+
 
 document.getElementById("btnAdd").addEventListener("click",function() {
     functions.add(num1.value,num2.value);
+});
+
+document.getElementById("btnSub").addEventListener("click",function() {
+    functions.subtract(num1.value,num2.value);
+});
     
 
+document.getElementById("btnMultiply").addEventListener("click",function() {
+    functions.multiply(num1.value,num2.value);
 });
+
+
+document.getElementById("btnDivide").addEventListener("click",function() {
+    functions.divide(num1.value,num2.value);
+});
+
+
+// export default functions;
