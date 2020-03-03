e81bb3a14cf1ea14c02c3bae01fb9330093c11c3
Shine <shine002@gmail.com>
2020-03-02 18:26:24 -0700

    fixing issue

--- 01-getting-started/src/scripts/tax.js
+++ 01-getting-started/src/scripts/tax.js
@@ -1,40 +1,20 @@
-let a = 48535;  //first tax slab
-let b = 48534;  //second  tax bracket
-let c = 53404;  //third  tax bracket
-let d = 63895;  //forth  tax bracket
+// var input = document.getElementById("input");
+// var button = document.getElementById("button");
+// var result = document.getElementById("result");
+
+
+// button.addEventListener("click",function() {
+//     console.log("works");
+// });
+
 
 const tax = {
 
-    taxCalculator: (num) => {   
-        if (num <= 48535){
-            return (num*15/100).toFixed(2);                  
-
-        }    
-             
-        else if (num>48535 && num<=97069){
-            return ((a*15/100) + (num-a)*20.5/100).toFixed(2);
-            
-        }    
-        
-        else if (num >97069 && num<=150473) {
-            return (a*15/100 + b*20.5/100 + (num-(a+b))*26/100).toFixed(2);
-            
-        }    
-        
-        else if (num > 150473 && num<=214368){        
-            return  (a*15/100 + b*20.5/100 + b*26/100 + (num-(a+b+c))*29/100).toFixed(2);
-            
-        } 
-        
-        else {
-            return (a*15/100 + b*20.5/100 + c*26/100 + d*29/100 + (num -(a+b+c+d))*33/100).toFixed(2);
-            
-        }
-        
+    taxCalculator: (num) => {
+        if (num <=48535) return (num/15*100);
+        if (num > 20) return "medium";
+        return "large";
     },
 
 };
-
-
-export default tax;
-
+git 
