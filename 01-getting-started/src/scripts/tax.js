3325a4c5243bb337eaee4151fe38dfdb3bef8fd9
Shine <shine002@gmail.com>
2020-03-02 18:31:56 -0700

    issue resolved

--- 01-getting-started/src/scripts/tax.js
+++ 01-getting-started/src/scripts/tax.js
@@ -1,20 +1,40 @@
-// var input = document.getElementById("input");
-// var button = document.getElementById("button");
-// var result = document.getElementById("result");
-
-
-// button.addEventListener("click",function() {
-//     console.log("works");
-// });
-
+let a = 48535;  //first tax slab
+let b = 48534;  //second  tax bracket
+let c = 53404;  //third  tax bracket
+let d = 63895;  //forth  tax bracket
 
 const tax = {
 
-    taxCalculator: (num) => {
-        if (num <=48535) return (num/15*100);
-        if (num > 20) return "medium";
-        return "large";
+    taxCalculator: (num) => {   
+        if (num <= 48535){
+            return (num*15/100).toFixed(2);                  
+
+        }    
+             
+        else if (num>48535 && num<=97069){
+            return ((a*15/100) + (num-a)*20.5/100).toFixed(2);
+            
+        }    
+        
+        else if (num >97069 && num<=150473) {
+            return (a*15/100 + b*20.5/100 + (num-(a+b))*26/100).toFixed(2);
+            
+        }    
+        
+        else if (num > 150473 && num<=214368){        
+            return  (a*15/100 + b*20.5/100 + b*26/100 + (num-(a+b+c))*29/100).toFixed(2);
+            
+        } 
+        
+        else {
+            return (a*15/100 + b*20.5/100 + c*26/100 + d*29/100 + (num -(a+b+c+d))*33/100).toFixed(2);
+            
+        }
+        
     },
 
 };
-git 
+
+
+export default tax;
+
