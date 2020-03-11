50e270f695e4b7a546b90efd3fdf35bf3da2ded2
SH!NE <shine002@gmail.com>
2020-03-11 13:32:48 -0600

    Dom Card still not working

---
+++ 02-dom/main.js
@@ -0,0 +1,32 @@
+import domfuncs from './dom.js'
+
+console.log(idButton);
+
+let counter = 0;
+
+document.body.addEventListener("click", e => {
+    // console.log("You Clicked, at your command");
+    // console.log(e.target.textContent);
+    // console.log(e.target.nodeName);
+    // console.log(e.target);
+
+    if (e.target.nodeName === 'BUTTON') {
+        // document.body.appendChild(domfuncs.buildCard("You just added " + counter++));
+        console.log(e.target.textContent);
+        if (e.target.textContent === "Add After") {
+            domfuncs.addBefore(e.target.parentElement, "You just added " + counter++);
+        }
+        
+    }
+
+    
+    
+
+    // const div = document.createElement("div");
+    // div.className = "clCard";
+    // div.appendChild(document.createTextNode("This has been inserted"));
+
+    // idStuff2.insertAdjacentElement("afterend", div);
+
+
+})
