d105a91d52a6536569053c3313e7885bfeb37f10
Shine <shine002@gmail.com>
2020-03-10 19:36:11 -0600

    DOM updated

---
+++ 02-dom/dom.js
@@ -0,0 +1,28 @@
+
+
+let addCard = document.getElementById("addCard");
+let div1 = document.querySelector("div") 
+addCard.addEventListener("click", function(){
+    console.log("Add card")
+    div1.appendChild(buildCard("New Card"));    
+})
+
+buildCard = (text) => {
+
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(text));
+
+    const add = document.createElement('button');
+    add.appendChild(document.createTextNode("Add"));
+    div.appendChild(add);
+
+    const before = document.createElement('button');
+    before.appendChild(document.createTextNode("Before"));
+    div.appendChild(before);
+
+    const del = document.createElement('button');
+    del.appendChild(document.createTextNode("Delete"));
+    div.appendChild(del);
+
+    return div;
+}
