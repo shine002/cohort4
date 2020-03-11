cc4186d7870cab207202a349abe2b51531d3fa9c
SH!NE <shine002@gmail.com>
2020-03-11 12:14:44 -0600

    Dom Card still not working

--- 02-dom/dom.js
+++ 02-dom/dom.js
@@ -7,9 +7,21 @@ addCard.addEventListener("click", function(){
     div1.appendChild(buildCard("New Card"));    
 })
 
+
+// document.body.addEventListener("click", e => {
+//        if (e.target.textContent === 'Add Before') {
+//         document.body.appendChild(domfuncs.buildCard("You just added " + counter++));
+//         console.log(e.target.textContent);
+//         if (e.target.c === "Add After") {
+//             buildCard(e.target.parentElement, "You just added " + counter++);
+//         }
+        
+//     }
+
 buildCard = (text) => {
 
     const div = document.createElement('div');
+    
     div.appendChild(document.createTextNode(text));
 
     const add = document.createElement('button');
