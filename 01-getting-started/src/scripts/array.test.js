14a15e25e3b7adf2f56bbcab8d7789b8dd845ed7
SH!NE <shine002@gmail.com>
2020-03-12 14:55:37 -0600

    test fixed

--- 01-getting-started/src/scripts/array.test.js
+++ 01-getting-started/src/scripts/array.test.js
@@ -11,9 +11,9 @@ test('Array test', () => {
     expect(array.createArray("3")).toEqual("Input Added");
     expect(array.clearArray()).toEqual("Array Cleared");
     expect(array.showArray()).toEqual("Array is Empty");
-    expect(array.createArray(9)).toEqual("Input added");
-    expect(array.createArray(4)).toEqual("Input added");
-    expect(array.createArray(4)).toEqual("Input added");
+    expect(array.createArray(9)).toEqual("Input Added");
+    expect(array.createArray(4)).toEqual("Input Added");
+    expect(array.createArray(4)).toEqual("Input Added");
     expect(array.showArray()).toEqual([9,4,4]);
     expect(array.addArray()).toEqual(17);
 
