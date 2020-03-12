6dadd8d7a233da6fa808a612a3455364f55846cb
SH!NE <shine002@gmail.com>
2020-03-12 14:53:52 -0600

    Array test fixed

--- 01-getting-started/src/scripts/array.test.js
+++ 01-getting-started/src/scripts/array.test.js
@@ -3,12 +3,12 @@ import array from "./array.js"
 
 test('Array test', () => {
     expect(array.createArray("test")).toEqual("Invalid Input");    
-    expect(array.createArray(5)).toEqual("Input added");
-    expect(array.createArray(2)).toEqual("Input added");
+    expect(array.createArray(5)).toEqual("Input Added");
+    expect(array.createArray(2)).toEqual("Input Added");
     expect(array.createArray("test")).toEqual("Invalid Input");
     expect(array.addArray()).toEqual(7);
     expect(array.createArray("")).toEqual("Invalid Input");
-    expect(array.createArray("3")).toEqual("Input added");
+    expect(array.createArray("3")).toEqual("Input Added");
     expect(array.clearArray()).toEqual("Array Cleared");
     expect(array.showArray()).toEqual("Array is Empty");
     expect(array.createArray(9)).toEqual("Input added");
