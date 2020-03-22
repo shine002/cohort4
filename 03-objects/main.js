eca949dbd88035dc921a126cbcb7ecf40b523309
Shine <shine002@gmail.com>
2020-03-21 20:25:27 -0600

    main.js updated

---
+++ 03-objects/main.js
@@ -0,0 +1,15 @@
+let aName = document.getElementById("aName"); //account name
+let iAmount = document.getElementById("iAmount"); //initial amount
+let addAccBtn = document.getElementById("addAccBtn"); //account add button
+let tAmount = document.getElementById("tAmount"); // transcation withdrawal and add amount 
+let withdraw = document.getElementById("withdraw"); //Withdraw button
+let deposit = document.getElementById("deposit"); // deposit button
+
+
+
+console.log(aName);
+console.log(iAmount);
+console.log(addAccBtn);
+console.log(tAmount);
+console.log(withdraw);
+console.log(deposit);
