351325eaf3afde36ef42d6b61cf6758dd1203307
unknown <shine002@gmail.com>
2020-03-26 13:06:12 -0600

    dir structure chnaged

---
+++ 03-objects/src/scripts/object.js
@@ -0,0 +1,19 @@
+export class Account {
+  constructor(accountName, initialBalance) {
+    this.accountName = accountName;
+    this.initialBalance = initialBalance;
+  }
+  deposit(amount) {
+    currentBalance = this.initialBalance + amount;
+  }
+
+  withdraw(amount) {
+    currentBalance = this.initialBalance - amount;
+  }
+
+  balance() {
+    return this.currentBalance;
+  }
+}
+
+// let chqAccount = new Account("Chq",23);
